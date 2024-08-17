be4a84cbb8a978e37328571c7e13755d61277c7d
Nguyen Trinh Vinh Thinh <thinhss123321@gmail.com>
2024-08-17 11:51:38 +0700

    feat: update controller

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,6 +1,6 @@
 import bcrypt from 'bcrypt'
 import nodemailer from 'nodemailer'
-import express, { Request, Response } from 'express'
+import express, { Request, response, Response } from 'express'
 import { ObjectId } from 'mongodb'
 import { json } from 'stream/consumers'
 import userSchema, { userType } from '~/models/user'
@@ -14,11 +14,10 @@ const authController = {
   //register
   register: async (req: Request, res: Response) => {
     try {
-      const valid = await authValidate.validateRegister(req, res)
-      console.log(valid)
+      const validationResult = await authValidate.validateRegister(req, res)
 
-      if (!valid) {
-        return res
+      if (validationResult.status != 200) {
+        return validationResult
       }
 
       console.log(req.body.email + 'in controller')
@@ -43,7 +42,7 @@ const authController = {
         )
         await sendVerificationEmail(newUser.email, newUser.name, accessToken)
         console.log('Please check your email')
-        res.status(201).json({ message: 'Please check your email' })
+        return res.status(201).json({ message: 'Please check your email' })
       } else {
         console.log('Can not send verification to your email')
         res.status(400).json({ message: 'Can not send verification to your email' })
@@ -65,12 +64,10 @@ const authController = {
       if (token) {
         jwt.verify(token, `${process.env.JWT_ACCESS_KEY}`, (err: any, userToken: any) => {
           if (err) {
-            res.status(403).json({ message: 'Token is not valid' })
+            return res.status(403).json({ message: 'Token is not valid' })
           } else {
             userVerifyEmail = userToken.email || ''
             userVerifyId = userToken._id || ''
-
-            console.log(userToken)
 
             // Use the verifiedEmail and verifiedID for further processing
             console.log('Verified Email:', userVerifyEmail)
@@ -91,17 +88,16 @@ const authController = {
 
   logIn: async (req: Request, res: Response) => {
     try {
-      const valid = await authValidate.validateLogin(req, res)
+      const validationResult = await authValidate.validateLogin(req, res)
 
-      console.log(valid)
-      if (!valid) {
-        return res
+      if (validationResult.status != 200) {
+        return validationResult
       }
 
       const user = await userSerive.findUserByEmail(req.body.email)
 
       const validPassword = await bcrypt.compare(req.body.password, user.password)
-      res.status(200).json('Login success')
+      return res.status(200).json('Login success')
     } catch (error) {
       return res.status(500).json({ message: 'Internal server error' })
     }
--- src/validates/authValidate.ts
+++ src/validates/authValidate.ts
@@ -11,80 +11,69 @@ import bcrypt from 'bcrypt'
 */
 
 const authValidate = {
-  validateRegister: async (req: Request, res: Response): Promise<boolean> => {
+  validateRegister: async (req: Request, res: Response): Promise<any> => {
     try {
+      //check valid email and password
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+      if (!emailRegex.test(req.body.email)) {
+        console.log('Invalid email')
+        return res.status(400).json({
+          message: 'Invalid email'
+        })
+      }
+
+      const passwordRegex = /^(?=.*[A-Z])(?=.*\d)[A-Za-z\d!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]{8,}$/
+      if (!passwordRegex.test(req.body.password)) {
+        console.log('Password must be at least 8 characters long, contain at least one uppercase letter and one number')
+        return res.status(400).json({
+          message: 'Password must be at least 8 characters long, contain at least one uppercase letter and one number'
+        })
+      }
+
       const checkedUser = await userService.findUserByEmail(req.body.email)
 
       if (!checkedUser) {
-        const passwordRegex = /^(?=.*[A-Z])(?=.*\d)[A-Za-z\d!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]{8,}$/
-        if (!passwordRegex.test(req.body.password)) {
-          res.status(400).json({
-            message: 'Password must be at least 8 characters long, contain at least one uppercase letter and one number'
-          })
-          console.log(
-            'Password must be at least 8 characters long, contain at least one uppercase letter and one number'
-          )
-          return false
-        }
+        return res.status(200)
+      }
+      console.log('Email already exists')
+      return res.status(400).json({ message: 'Email already exists' })
 
-        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/        
-        if (!emailRegex.test(req.body.email)) {
-          res.status(400).json({
-            message: 'Invalid email'
-          })
-          console.log(
-            'Invalid email'
-          )
-          return false
-        }
-        return true
-      }console.log(
-        'Email already exists'
-      )
-      res.status(400).json({ message: 'Email already exists' })
-      return false
     } catch (error) {
       console.log(error)
-      res.status(500).json({
+      return  res.status(500).json({
         message: 'Internal server error'
       })
-      return false
     }
   },
 
-  validateLogin: async (req: Request, res: Response) => {
+  validateLogin: async (req: Request, res: Response): Promise<any> => {
     try {
       if (req.body.email === '' || req.body.password === '') {
         console.log('Please fill in your email and password')
-        res.status(404).json({ message: 'Please fill in your email and password' })
-        return false
+        return res.status(404).json({ message: 'Please fill in your email and password' })
       }
       const checkedUser = await userService.findUserByEmail(req.body.email)
-      
+
       if (!checkedUser) {
-        console.log('Wrong email')
-        res.status(404).json({ message: 'Wrong email or password' })
-        return false
+        console.log('Wrong email or password')
+        return res.status(404).json({ message: 'Wrong email or password' })
       }
       const validPassword = await bcrypt.compare(req.body.password, checkedUser.password)
 
       if (!validPassword) {
-        console.log('Wrong password')
-        res.status(400).json({ message: 'Wrong email or password' })
-        return false
+        console.log('Wrong email or password')
+        return res.status(400).json({ message: 'Wrong email or password' })
       }
 
       if (checkedUser.verifiedEmailToken === '') {
         console.log('Please verify your email, remember to check in trash emails')
-        res.status(404).json({ message: 'Please verify your email, remember to check in trash emails' })
-        return false
+        return res.status(404).json({ message: 'Please verify your email, remember to check in trash emails' })
       }
 
-      return true
+      return res.status(200)
     } catch (error) {
       console.log(error)
-      res.status(500).json({ message: 'Internal server error' })
-      return false
+      return res.status(500).json({ message: 'Internal server error' })
     }
   }
 }
